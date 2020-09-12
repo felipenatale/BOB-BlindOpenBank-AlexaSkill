812ef7f1e88a27275ed069348406a21f83028410
wilfaustino <wilfaustino@gmail.com>
2020-09-12 01:49:00 -0300

    Modificações no ponto de entrada 'Launch' e no 'Balance Handler'

--- lambda/request-handlers/BalanceIntentHandler.js
+++ lambda/request-handlers/BalanceIntentHandler.js
@@ -1,20 +1,33 @@
 const Alexa = require('ask-sdk-core');
 const msg = require('../app-messages');
+const { getAttr, clearAttr } = require('../utils/sessionHandler.js');
+const sayMoneyNumber = require('../utils/sayMoneyNumber');
 
 module.exports = {
     canHandle(handlerInput) {
         
-        const yesIntentFromLaunch = Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.YesIntent';
+        //From LaunchIntent and said 'Sim'
+        const yesIntentFromLaunch = Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.YesIntent' && getAttr(handlerInput, 'lastQuestion') === 'consulta-saldo-launch';
+        
+        //From ConsultaSaldo intent
         const intentBalance = Alexa.getIntentName(handlerInput.requestEnvelope) === 'ConsultaSaldo';
         
+        //Clear lastQuestion
+        if(yesIntentFromLaunch){
+            clearAttr(handlerInput, 'lastQuestion');
+        }
+
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' && (yesIntentFromLaunch || intentBalance);
         
     },
     handle(handlerInput) {
+        
+        //Consultar saldo
+        let balance = -1050.55;
 
         return handlerInput.responseBuilder
-            .speak("Seu saldo é de 0 reais!")
-            .reprompt("Deseja mais alguma ação?")
+            .speak('Seu saldo está ' + sayMoneyNumber(balance) + '!')
+            .reprompt(msg.whatElseHelp)
             .getResponse();
             
     }
--- lambda/request-handlers/LaunchRequestHandler.js
+++ lambda/request-handlers/LaunchRequestHandler.js
@@ -1,12 +1,17 @@
 const Alexa = require('ask-sdk-core');
 const msg = require('../app-messages');
+const { modifyAttr } = require('../utils/sessionHandler.js');
 
 module.exports = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-       
+        
+        //Login
+        
+        modifyAttr(handlerInput, 'lastQuestion', 'consulta-saldo-launch');
+
         return handlerInput.responseBuilder
             .speak(msg.welcomeMsg + msg.getBalance)
             .reprompt(msg.getBalance)
