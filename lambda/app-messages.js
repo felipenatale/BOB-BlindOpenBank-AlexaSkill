882944880b43530f0f8b1025128d3e98f671e291
wilfaustino <wilfaustino@gmail.com>
2020-09-12 01:49:59 -0300

    Acrescentadas mensagens

--- lambda/app-messages.js
+++ lambda/app-messages.js
@@ -1,6 +1,12 @@
 module.exports = {
-    erroMsg: 'Sorry, I had trouble doing what you asked. Please try again.',
-    welcomeMsg: 'Olá! Estou ansiosa para poder me integrar ao BOB e fazer transações bancárias!',
+    
+    welcomeMsg: 'Olá! Seja bem-vindo ao BOB, o Blind Open Bank!',
+    erroMsg: 'Desculpe, algum problema técnico aconteceu! Tente novamente!',
     getBalance: 'Você deseja consultar seu saldo?',
-    generalHelp: 'Você pode me pedir para consultar seu saldo ou para realizar uma transferência!'
+    generalHelp: 'Você pode me pedir para consultar seu saldo ou para realizar uma transferência!',
+    whatElseHelp: 'Em que mais posso te ajudar?',
+    emptyBalance: 'zerado',
+    negativaBalance: 'negativo em',
+    positiveBalance: 'positivo em'
+    
 }
