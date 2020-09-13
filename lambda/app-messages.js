9fbddf8d572bae4dbe9caae96b92fd28ce1e3b7e
wilfaustino <wilfaustino@gmail.com>
2020-09-12 23:08:53 -0300

    Atualizadas as msgs

--- lambda/app-messages.js
+++ lambda/app-messages.js
@@ -1,18 +1,28 @@
 module.exports = {
     
-    welcomeMsg: 'Olá! Seja bem-vindo ao BÓB, Blind Open Bank!',
+    welcomeMsg: 'Olá! Seja bem-vindo ao BÓB <lang xml:lang="en-US">Blind Open Bank</lang>!',
+    sayTransactions: 'Para consultar seu extrato, diga: extrato',
+    transacrionsEnd: 'Fim do extrato.',
+    msgPassword: 'Diga sua senha de quatro dígitos',
+    authFirst: 'Primeiro você precisa autenticar-se!',
     erroMsg: 'Desculpe, algum problema técnico aconteceu! Tente novamente!',
+    fallBack: 'Desculpe, não consegui entender. Tente novamente, por favor!',
     getBalance: 'Você deseja consultar seu saldo?',
     generalHelp: 'Você pode me pedir para consultar seu saldo ou para realizar uma transferência!',
     whatElseHelp: 'Em que mais posso te ajudar?',
     notCompreensive: 'Humm... acho que não entendi, você pode repetir?',
-    hiThere: 'Olá? Você ainda está ai? Se quiser ajuda, diga "Ajuda"',
-    thanksForUsing: 'Obrigada por utilizar o BOB, Blind Open Bank',
+    hiThere: 'Olá? Você ainda está aí? Se quiser ajuda, diga "Ajuda"',
+    thanksForUsing: 'Obrigada por utilizar o BÓB <lang xml:lang="en-US">Blind Open Bank</lang>',
     emptyBalance: 'zerado',
     negativaBalance: 'negativo em',
     positiveBalance: 'positivo em',
     openingMorningCalls: 'Abrindo seus <lang xml:lang="en-US">Morning Calls</lang> de hoje...',
-    yoursTransactionsOf: 'Aqui está seu extrato de',
-    thereIsNoTransactions: 'Não há lançamentos'
+    yourTransactionsOf: 'Aqui está seu extrato de',
+    yourTransactions: 'Aqui está seu extrato',
+    thereIsNoTransactions: 'Não há lançamentos',
+    enabledBanks: 'Este dispositivo está habilitado para o melhor banco do mundo: O Banco Safra!',
+    correctPassword: 'Sua senha está correta!',
+    wrongPassword: 'Senha incorreta! Até mais...',
+    howCanIHelp: 'Como posso te ajudar?'
     
 }
